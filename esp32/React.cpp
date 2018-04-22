23e5ac33d7daeadfdd755275b15535c378195ea7
Cinek28 <marcinb280@op.pl>
2018-04-22 04:40:44 +0200

    Adding react game

--- esp32/React.cpp
+++ esp32/React.cpp
@@ -1,39 +1,42 @@
 #include "React.h"
-#include "LEDDriver.h"
+#include <random>
 
-#include <Arduino.h>
-
-unsigned long React::max_waiting_time = 20000;
+const unsigned long React::max_waiting_time = 20000;
 
 React::React(int players_cnt)
-  : Game(players_cnt),
-  time_stamp(0),
-  state(State::GENERATE_COLOUR),
-  colour(0)
+        : Game(players_cnt),
+          time_stamp(0),
+          state(State::GENERATE_COLOUR),
+          colour(0)
 {}
 
 bool React::continue_game() {
-  switch(state) {
-  case State::GENERATE_COLOUR:
-     // get random colour
-    colour = 0;
-    led_driver.single_blink(50, LEDColour::BLUE);
-    time_stamp = millis();
-    state = State::WAIT_FOR_REACTION;
-    break;
-  case State::WAIT_FOR_REACTION: {
-    unsigned long time_diff = millis() - time_stamp;
-    if (distance_sensor[colour].is_activated() || time_diff > max_waiting_time) {
-      // update player points table
-      // check game over condition
-      state = State::FINALIZE;
-    }
-    break;
-  }
-  case State::FINALIZE:
-    // check if next game
-    // go to base state
-    break;
-  }
-}
+    switch(state) {
+        case State::GENERATE_COLOUR:
+            std::default_random_engine generator;
+            std::uniform_int_distribution<int> distribution(0,3);
+            colour = distribution(generator);
+            led_driver.single_blink(50, static_cast<LEDColour>(colour));
+            time_stamp = millis();
+            state = State::WAIT_FOR_REACTION;
+            break;
+        case State::WAIT_FOR_REACTION
+            unsigned long time_diff = millis() - time_stamp;
+            for(int i=0; i < 4; ++i){
+              if(distance_sensor[static_cast<LEDColour>(i)].is_activated()){
+                
+              }
+            }
+            if(time_diff > max_time_diff)
+              state = State::UPDATE_SCORES;
+            break;
+        case State::UPDATE_SCORES:
+            
 
+        case State::FINALIZE:
+            // check if next game
+            // go to base state
+            break;
+    }
+    return false;
+}
