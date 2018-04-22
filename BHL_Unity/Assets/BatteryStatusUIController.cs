b3cbd6e6c074db3a9a830470ec57cde72dd7a070
Piotr Sykulski <pitersk@gmail.com>
2018-04-22 03:03:32 +0200

    Fixed bug in UI, fixed icons

---
+++ BHL_Unity/Assets/BatteryStatusUIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryStatusUIController : MonoBehaviour {
+
+	public Sprite batteryFull;
+	public Sprite batteryNearlyFull;
+	public Sprite batteryHalf;
+	public Sprite batteryNearlyEmpty;
+	public Sprite batteryEmpty;
+
+	public Image batteryImage;
+
+	public void setBatteryLevel(int level){
+		if (level == 0) {
+			batteryImage.sprite = batteryEmpty;
+		} else if (level == 1) {
+			batteryImage.sprite = batteryNearlyEmpty;
+		} else if (level == 2) {
+			batteryImage.sprite = batteryHalf;
+		} else if (level == 3) {
+			batteryImage.sprite = batteryNearlyFull;
+		} else if (level == 4) {
+			batteryImage.sprite = batteryFull;
+		}
+	}
+
+}
